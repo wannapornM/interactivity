73660d81dd3ca0265fa6b52d1a1bb292a1b19107
wannapornd <wannapornd@hellospec.com>
2022-06-07 16:43:59 +0700

    Example of remove unnecessary state

--- src/components/FeedbackForm.js
+++ src/components/FeedbackForm.js
@@ -1,27 +1,10 @@
-import { useState } from 'react';
-
 export default function FeedbackForm() {
-  const [isSent, setIsSent] = useState(false);
-  const [message, setMessage] = useState('');
-  
-  if (isSent) {
-    return <h1>Thank you!</h1>;
-  } else {
+    function handleClick() {
+        const name = prompt("What is your name?")
+        alert(`Hello ${name}!`)
+    }
+
     return (
-      <form onSubmit={e => {
-        e.preventDefault();
-        alert(`Sending: "${message}"`);
-        setIsSent(true);
-      }}>
-        <textarea
-          className="border"
-          placeholder="Message"
-          value={message}
-          onChange={e => setMessage(e.target.value)}
-        />
-        <br />
-        <button className="bg-blue-200" type="submit">Send</button>
-      </form>
-    );
-  }
+        <button onClick={handleClick}>Greet</button>
+    )
 }
