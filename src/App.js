03e427c1de55592a687fd4f2c2017232d79f3a2b
wannapornd <wannapornd@hellospec.com>
2022-06-07 10:30:06 +0700

    Test event handler

--- src/App.js
+++ src/App.js
@@ -1,3 +1,24 @@
 export default function App() {
-    return <h1 className="text-blue-400">Hello from app.js</h1>;
+  function Toolbar(props) {
+    const { onPlayMovie, onUploadImage } = props
+
+    return (
+      <div>
+        <Button onClick={onPlayMovie}>Play Movie</Button>
+        <Button onClick={onUploadImage}>Upload Image</Button>
+      </div>
+    )
   }
+
+  function Button(props) {
+    const { onClick, children } = props
+
+    return (
+      <button onClick={onClick}>{children}</button>
+    )
+  }
+
+  return (
+    <Toolbar onPlayMovie={() => alert("Playing!")} onUploadImage={() => alert("Uploading!")} />
+  )
+}
